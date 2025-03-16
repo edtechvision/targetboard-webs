b283c4d6767a24b2699a91f06365ddc35f667737
codex-anki09 <codexankita@gmail.com>
2025-03-16 13:00:58 +0530

    changed node package to 21

---
+++ config/env/production/server.js
@@ -0,0 +1,8 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', process.env.PORT || 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  url: env('PUBLIC_URL'),
+});
