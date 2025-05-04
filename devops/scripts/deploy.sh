d24acb7abb3a351631077754fc34d47e928663a9
codex-anki09 <codexankita@gmail.com>
2025-05-04 08:23:04 +0000

    Deployment

--- devops/scripts/deploy.sh
+++ devops/scripts/deploy.sh
@@ -7,21 +7,23 @@ REMOTE_PASSWORD="yu2iu9s00XWaMnf"
 CODE_LOCATION="/root/targetboard-webs"
 
 # SSH into the remote machine and execute commands
-sshpass -p $REMOTE_PASSWORD ssh $REMOTE_USER@$REMOTE_HOST << EOF
+sshpass -p "$REMOTE_PASSWORD" ssh -tt -o StrictHostKeyChecking=no -o LogLevel=ERROR "$REMOTE_USER@$REMOTE_HOST" << 'EOF'
+  set -e  # Exit on any error
+
   # Load NVM environment for the root user
   export NVM_DIR="/root/.nvm"
-  [ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
   # Change to the code directory
-  cd $CODE_LOCATION
+  cd /root/targetboard-webs
 
   # Pull the latest code
   git pull
-  git pull
 
   # Install dependencies
-  npm i
+  npm install
 
   # Restart the backend using pm2
   pm2 restart backend
 EOF
+
